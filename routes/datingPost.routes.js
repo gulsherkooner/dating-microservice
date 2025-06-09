f279b2cd07c8636a600127a9d171fd931bdde701
Cyros-Sachin <sachinkc4456@gmail.com>
2025-06-09 23:02:52 +0530

    dating posts

--- routes/datingPost.routes.js
+++ routes/datingPost.routes.js
@@ -4,7 +4,7 @@ import { extractUserFromHeader } from '../middleware/extractUser.js';
 
 const router = express.Router();
 
-router.post('/dating-posts', extractUserFromHeader, createPost);
+router.post('/dating-posts/me', extractUserFromHeader, createPost);
 router.get('/dating/me', extractUserFromHeader, getUserPosts);
 router.patch('/dating-posts/pin-post/:id', extractUserFromHeader, updatePinStatus);
 
